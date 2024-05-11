622493516b2e56d9840476fd1e58a452671ce7ef
Isokayy <fareezdaniel34@gmail.com>
2024-05-11 17:17:49 +0800

    final test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { AdviceCard } from './AdviceCard';
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+    <AdviceCard></AdviceCard>
+    </div>
+    
+  );
+}
+
+export default App;
